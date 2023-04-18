65e4a84b63cab25d3c8ac8dc59dff84762c31405
hong-yeong-gwang <chamsk0808@naver.com>
2023-04-18 10:19:56 +0900

    vuetify 설정

--- ybus_front/src/busInfoPage/main.js
+++ ybus_front/src/busInfoPage/main.js
@@ -4,8 +4,10 @@ import router from './router/index.js';
 import "../style.css";
 import axios from 'axios'
 
+
+
 const app = createApp(busInfo)
 
-app. config.globalProperties.axios = axios;
+app.config.globalProperties.axios = axios;
 app.config.productionTip = false
 app.use(router).mount('#app')
