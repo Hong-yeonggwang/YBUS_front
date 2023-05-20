da313dd34048fa6ade764313b0bc0efcb715684e
doyounglee <eeu1234@naver.com>
2023-05-20 19:41:37 +0900

    관리자페이지 레이아웃 수정 시작

--- ybus_front/src/components/AdminFooter.vue
+++ ybus_front/src/components/AdminFooter.vue
@@ -1,14 +1,10 @@
 <template>
-
-<div class="w-full footerBackgroundColor text-white">
-  <div class="logo"></div>
-  <div class="text-xs Pretendard-SemiBold">이전 버전으로</div>
-  <div class="info">
-    <div class="float-left Pretendard-Medium">The UNIV BUS::YBUS made by YBUS Team<br>
-    2022 All right (C) reserved.</div>
-    <div class="float-right maillogo"></div>
-    <div class="float-right kakaologo"></div>
-    <div class="clear-both"></div>
+<div class="w-full h-15 bg-slate-600 text-white">
+  <div class="logo w-full "><img :src="require('@/assets/ybus_style_resource/ybus_img/mainpage/YBUS_footer.png')"></div>
+  <div class="info w-full ">
+    <div class="w-full">The UNIV BUS::YBUS made by YBUS Team    2022 All right (C) reserved.</div>
+    <div class="maillogo"></div>
+    <div class="kakaologo"></div>
   </div>
 </div>
 
@@ -25,31 +21,12 @@
   </script>
 
   <style>
-    .footerBackgroundColor{
-      background-color: #71727B ;
-      padding: 0 5%;
 
-    }
-    .logo{
-      content:url(@/assets/ybus_style_resource/ybus_img/mainpage/YBUS_footer.png);
-      padding: 7.2% 0;
-      width: 23.2%;
-    }
     .kakaologo{
       content:url(@/assets/ybus_style_resource/ybus_img/mainpage/kakao.png);
-      width: 10%;
-      margin-right: 2.5%;
     }
     .maillogo{
       content:url(@/assets/ybus_style_resource/ybus_img/mainpage/gmail_logo.png);
-      width:10%;
     }
-    .info{
-      width: 100%;
-      padding-top: 12.8px;
-      padding-bottom: 25.6px;
-      font-family: "Pretendard-Medium";
-      font-size: 0.75rem;
-      letter-spacing: -0.5px;
-    }
+
   </style>
